43e0dff29c47f2ea22b0a7bc8728e297a2a2b2b6
Felix <38662557+StartNight@users.noreply.github.com>
2020-05-18 09:54:45 +0800

    Update MainActivity.java

--- AndroidProject/u2a/src/main/java/com/xiaoma/u2a/MainActivity.java
+++ AndroidProject/u2a/src/main/java/com/xiaoma/u2a/MainActivity.java
@@ -10,4 +10,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+     //unity调用Android
+    public void UnityCallAndroid () {
+
+        Toast.makeText(this,"unity调用android成功", Toast.LENGTH_LONG).show();
+
+        AndroidCallUnity();
+    }
+
+    //android调用unity
+    public void AndroidCallUnity () {
+
+        //第1个参数为Unity场景中用于接收android消息的对象名称
+        //第2个参数为对象上的脚本的一个成员方法名称（脚本名称不限制）
+        //第3个参数为unity方法的参数
+        UnityPlayer.UnitySendMessage("receiveObj", "UnityMethod", "This is args.");
+    }
 }
